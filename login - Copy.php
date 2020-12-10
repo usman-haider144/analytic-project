c1030479bca593f3f07505122a5c6cc9b9fd357d
Usman Haider <75796479+usman-haider144@users.noreply.github.com>
2020-12-10 22:10:46 +0500

    Create login - Copy.php

---
+++ login - Copy.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<?php 
+ //Template Name: login
+ // Template Post Type: page
+
+ ?>
+ <div class="container mt-5">
+ 	<?php  echo do_shortcode("[pmpro_login]");  ?>
+ </div>
+  <?php get_footer(); ?>
