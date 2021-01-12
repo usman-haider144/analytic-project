2f66d3f0eced959d674787bc52046045cdd0c3c4
Usman Haider <75796479+usman-haider144@users.noreply.github.com>
2021-01-13 00:29:49 +0500

    Update loadmore.js

--- js/loadmore.js
+++ js/loadmore.js
@@ -1,27 +1,23 @@
-jQuery(function($){
+jQuery(function($) {
 
 
-$(".misha_loadmore").click(function()
-{
-    $.post('http://localhost/analytic/wp-admin/admin-ajax.php',
-    {
-      'action': 'your_load_more',
-      'count': $(".bg-bord > div > .bg-post").length
-    },
-    function(response)
-    {
-      var posts = JSON.parse(response);
+    $(".misha_loadmore").click(function() {
+        $.post('http://localhost/analytic/wp-admin/admin-ajax.php', {
+                'action': 'your_load_more',
+                'count': $(".bg-bord > div > .bg-post").length
+            },
+            function(response) {
+                var posts = JSON.parse(response);
 
-      for( var i = 0; i < posts.length; i++ )
-      {
-        if( posts[i] == "0" )
-          $(".misha_loadmore").fadeOut();
-        else
-          $(".misha_loadmore").before(posts[i]);
-      }
+                for (var i = 0; i < posts.length; i++) {
+                    if (posts[i] == "0")
+                        $(".misha_loadmore").fadeOut();
+                    else
+                        $(".misha_loadmore").before(posts[i]);
+                }
 
+            });
     });
-});
 
 
 });
